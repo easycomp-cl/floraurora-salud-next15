5ab1469394c2e2eb7b2092e12d7737a69f807c6f
Diego <rafaelgonz200@gmail.com>
2025-08-23 21:21:58 -0400

    Actualización pagina de inicio

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,27 @@
+// Ejemplo en una página Next.js/React
+import Hero from "../../../src/components/layout/Hero";
+
 export default function HomePage() {
   return (
-    <div className="min-h-screen">
-      <p className="text-center text-2xl font-bold mt-20">
-        Desarrollar Home Page
-      </p>
-    </div>
+    <main>
+      <Hero imageSrc="/images/terapia-grupal.jpg" onCtaClick={() => {
+        // navega o abre modal
+        window.location.href = "/reservar";
+      }} />
+    </main>
   );
 }
+
+
+
+/*export default function HomePage() {
+  return (
+    <main>
+    <div className="min-h-screen">
+      <p className="text-center text-2xl font-bold mt-20">
+        Desarrollar Home 
+      </p>
+    </div>
+    </main>
+  );
+}*/
